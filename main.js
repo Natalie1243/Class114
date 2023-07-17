7f56b65e18d08cf05c36b4a59ac883267da40c39
natalie <113218013+Natalie1243@users.noreply.github.com>
2023-07-17 10:46:52 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    camvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function tale_snapshot(){
+    save('myFilterImage.png');
+}
